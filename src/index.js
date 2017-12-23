c0e15cd1c536b93fa8b53e2aaf84bab869523723
felixcatto <felix0808@yandex.ru>
2017-12-23 11:07:47 +0400

    fix second task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import fs from 'fs';
 import _ from 'lodash';
-import Tree from './tree';
 
 
 const formatToParser = {
@@ -11,72 +10,62 @@ export const defaultFormat = 'json';
 export const getFileContent = path => fs.readFileSync(path).toString();
 
 const makeAst = (objBefore, objAfter) => {
-  const iter = (tree, parentsKeys, before, after) => {
-    const beforeKeys = Object.keys(before);
-    const afterKeys = Object.keys(after);
-    const unionKeys = _.union(beforeKeys, afterKeys).sort();
-    return unionKeys.reduce((acc, key) => {
-      if (beforeKeys.includes(key) && !afterKeys.includes(key)) {
-        return acc.addChild(parentsKeys, key, {
-          key,
-          oldValue: before[key],
-          newValue: null,
-          type: 'deleted',
-        });
-      } else if (!beforeKeys.includes(key) && afterKeys.includes(key)) {
-        return acc.addChild(parentsKeys, key, {
-          key,
-          oldValue: null,
-          newValue: after[key],
-          type: 'added',
-        });
-      } else if (before[key] !== after[key]) {
-        return acc.addChild(parentsKeys, key, {
-          key,
-          oldValue: before[key],
-          newValue: after[key],
-          type: 'modified',
-        });
-      } else if (before[key] === after[key]) {
-        return acc.addChild(parentsKeys, key, {
-          key,
-          oldValue: before[key],
-          newValue: after[key],
-          type: 'not-modified',
-        });
-      }
-      throw new Error('impossible case');
-    }, tree);
-  };
-  return iter(new Tree('root', null), ['root'], objBefore, objAfter);
+  const beforeKeys = Object.keys(objBefore);
+  const afterKeys = Object.keys(objAfter);
+  const unionKeys = _.union(beforeKeys, afterKeys).sort();
+  return unionKeys.reduce((acc, key) => {
+    if (beforeKeys.includes(key) && !afterKeys.includes(key)) {
+      return [...acc, {
+        key,
+        oldValue: objBefore[key],
+        newValue: null,
+        type: 'deleted',
+      }];
+    } else if (!beforeKeys.includes(key) && afterKeys.includes(key)) {
+      return [...acc, {
+        key,
+        oldValue: null,
+        newValue: objAfter[key],
+        type: 'added',
+      }];
+    } else if (objBefore[key] !== objAfter[key]) {
+      return [...acc, {
+        key,
+        oldValue: objBefore[key],
+        newValue: objAfter[key],
+        type: 'modified',
+      }];
+    } else if (objBefore[key] === objAfter[key]) {
+      return [...acc, {
+        key,
+        oldValue: objBefore[key],
+        newValue: objAfter[key],
+        type: 'not-modified',
+      }];
+    }
+    throw new Error('impossible case');
+  }, []);
 };
 
 const render = (ast) => {
-  const iter = (acc, tree) => {
-    const data = tree.getData();
-    if (tree.isRoot()) {
-      return tree.getChildren()
-        .map(child => iter('', child))
-        .join('');
-    }
+  const rows = ast.reduce((acc, row) => {
     const {
       key,
       type,
       newValue,
       oldValue,
-    } = data;
+    } = row;
     if (type === 'added') {
-      return `  + ${key}: ${newValue}\n`;
+      return `${acc}  + ${key}: ${newValue}\n`;
     } else if (type === 'deleted') {
-      return `  - ${key}: ${oldValue}\n`;
+      return `${acc}  - ${key}: ${oldValue}\n`;
     } else if (type === 'modified') {
-      return `  - ${key}: ${oldValue}\n  + ${key}: ${newValue}\n`;
+      return `${acc}  - ${key}: ${oldValue}\n  + ${key}: ${newValue}\n`;
     } else if (type === 'not-modified') {
-      return `    ${key}: ${newValue}\n`;
+      return `${acc}    ${key}: ${newValue}\n`;
     }
     throw new Error('impossible case');
-  };
-  const rows = iter('', ast);
+  }, '');
   return `{\n${rows}}\n`;
 };
 
